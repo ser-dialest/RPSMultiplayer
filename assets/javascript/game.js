1241f74ef489fe8a0c1cc630dde5f62b4e9578c8
Dialest <dialest@hotmail.com>
2019-01-31 00:31:47 -0800

    Most of the basic ugly JS is written, byt the FB is a mess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,8 @@ firebase.initializeApp(config);
 // Let's do it. Declare the database
 var database = firebase.database();
 
+var wins = 0;
+var losses = 0;
 
 // function for logging in
 function loginScreen() {
@@ -34,7 +36,7 @@ function loginScreen() {
         if (input.val() != "") {
             // FIREBASE COMPONENT - check if p1 has been declared yet ****
             // set p1Name
-            database.ref().set({
+            database.ref().update({
                 p1Name: input.val()
             });
         };
@@ -43,6 +45,67 @@ function loginScreen() {
         // Move to Army Select
         chooseArmy();
     })
+}
+
+function chooseArmy(){
+    // clear the screen
+    $("body").html("");
+    $("body").css({"height":"300px", "display": "flex","justify-content": "space-around", "align-items": "center"})
+    // Make boxes
+    function armyBox(color) {
+        // create a box
+        var box = $("<div>");
+        // give it a class for clicking and an idea for color selection 
+        box.attr({"class": "army", "id": color});
+        box.css({"height": "100px", "width": "100px", "background": color})
+        return box;
+    }
+    // make the boxes
+    $("body").append(armyBox("orange"));
+    $("body").append(armyBox("blue"));
+    $("body").append(armyBox("yellow"));
+    $("body").append(armyBox("green"));
+    // make the armies selectable
+    $(".army").on("click", function () {
+        console.log($(this).attr("id"));
+        database.ref().update({
+            p1Army: $(this).attr("id")
+        });
+        loadGame();
+    })
+}
+
+function loadGame() {
+    // clear the screen
+    $("body").html("");
+    // creat play are ****
+    $("body").css({"height":"600px", "display": "flex","justify-content": "column", "align-items": "center"})
+
+    $("body").append("<div>MAP</div>");
+    function choiceBox(choice) {
+        var box = $("<div>");
+        // give it a class for clicking and an idea for color selection 
+        box.attr({"class": "choice", "id": choice});
+        box.css({"height": "100px", "width": "100px",})
+        box.text(choice);
+        return box;
+    }
+    // Add choices
+    $("body").append(choiceBox("rock"));
+    $("body").append(choiceBox("paper"));
+    $("body").append(choiceBox("scissors"));
+
+    // create victory status
+    $("body").append("<div>VICTORY AREA</div>");
+
+    $(".army").on("click", function () {
+        console.log($(this).attr("id"));
+        database.ref().update({
+            p1Army: $(this).attr("id")
+        });
+        loadGame();
+    })
+
 }
 
 loginScreen();
